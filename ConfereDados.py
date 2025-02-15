e0d9ba9170f9be6c1a8be3b1a3faf72df5a8b34e
LiBKun <65745956+LiBKun@users.noreply.github.com>
2025-02-15 13:48:46 -0300

    Update ConfereDados.py

--- ConfereDados.py
+++ ConfereDados.py
@@ -1,3 +1,34 @@
+# Conexão Sheets
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Planilhas
+from openpyxl import load_workbook
+import pandas as pd
+
+# Arquivos
+import os
+from pathlib import Path
+
+# CONEXÃO COM GOOGLE SHEETS
+filename = "ARQUIVO_G_SHEET.json" # ARQUIVO GERADO NO GOOGLE CLOUD PELAS CONTAS DE SERVIÇO
+
+    # PADRÃO
+scopes = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+    # PADRÃO
+creds = ServiceAccountCredentials.from_json_keyfile_name(filename=filename,scopes=scopes)
+client = gspread.authorize(creds)
+
+
+titulo = "NOME_PLANILHA" # NOME DO SHEET
+folder_id = "ID_PASTA_DRIVE" # ID DA PASTA NO GOOGLE DRIVE
+
+    # PADRÃO
+planilhaCompleta = client.open(title = titulo, folder_id = folder_id)
 
 origem = r"C:\Users\Padrão\Desktop\BeL Tech\VJ\Royal\Planilhas"
 for caminho, subpasta, arquivos in os.walk(origem):
